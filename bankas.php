3fdf4ac8202e0e308845b2951b823af880cd448d
Beata Rynkevič <beata.rynkevic1@gmail.com>
2021-03-02 20:04:17 +0200

    Inital code

---
+++ bankas.php
@@ -0,0 +1,5 @@
+//pirma karta aplikacija tuscia, nuskaito jsno,faila, jeigu neranda, sukuria tuscia masyva ir sukuria i user,json_decode
+//03.01 paskaitos yra pvz kaip nuo nulio
+//jeigu i saskaitu atvazdavimo puslapi, tai jis kreipiasi i json
+//atvaizduojam ka turi , jeigu ne,tai atvaizduojam tuscia. Logika virsuje tureti
+//pacioje pradzioje, mini crud tapati probelma bus 03.02 paskaita
