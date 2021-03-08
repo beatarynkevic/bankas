70ffdd2762beeda855960704623b5566705f6ab8
Beata Rynkevič <beata.rynkevic1@gmail.com>
2021-03-08 08:05:47 +0200

    edit

--- nesmones.php
+++
@@ -1,8 +0,0 @@
-<div class="mygtukai">
-        <!-- <button class="btn btn-primary" type="submit">Naujas mokėjimas</button> -->
-        <a class="btn btn-success" href="<?= URL ?>create.php">Kurti nauja saskaita</a>
-            <!-- atsijungimo linkas. Kreipiames i logino pslp,  -->
-        <a class="btn btn-dark" role="button" href="<?= URL?>login.php?logout">Atsijungti</a>
-        <!-- <button class="btn btn-warning" type="submit">Nusaičiuoti lėšas</button>
-        <button class="btn btn-danger" type="submit">Ištrinti</button> -->
-    </div>
